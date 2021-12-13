5807be3c06bc97b2127b2ef262c6aba1e3b1a72f
CHIMMILI <muralichimmili@gmail.com>
2021-12-13 18:59:01 +0530

    Added new files to the repository

--- createinstances.sh
+++ createinstances.sh
@@ -6,12 +6,12 @@ UPDATE_DNS_RECORDS() {
     sed -e "s/DNSNAME/$1-dev.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
     aws route53 change-resource-record-sets --hosted-zone-id Z04529243PF3H80RPJQ0J --change-batch file:///tmp/record.json | jq  &>/dev/null
 }
-
+# "ResourceType=instance,Tags=[{Key=Name,Value=$1}]"
 CREATE() {
   COUNT=$(aws ec2 describe-instances --filters  "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null  | wc -l)
 
   if [ $COUNT -eq 0 ]; then
-  aws ec2 run-instances --launch-template LaunchTemplateId=lt-0dc3347829b90e6c4 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$1}]" | jq &>/dev/null
+  aws ec2 run-instances --launch-template LaunchTemplateId=lt-0dc3347829b90e6c4 --tag-specifications  "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$1}]" | jq &>/dev/null
  #aws ec2 run-instances --launch-template LaunchTemplateId=lt-0dc3347829b90e6c4 "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$1}]" | jq &>/dev/null
   else
     echo -e "\e[1;33m$1 Instance already exists\e[0m"
