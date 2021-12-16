1915d9fcc52ca3507b3323be2fb787b04fac3dd7
CHIMMILI <muralichimmili@gmail.com>
2021-12-16 10:15:52 +0530

    Added new files to the repository

--- createinstances.sh
+++ createinstances.sh
@@ -3,15 +3,15 @@
 UPDATE_DNS_RECORDS() {
     IP=$(aws ec2 describe-instances --filters  "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null )
     ## xargs is used to remove the double  quotes
-    sed -e "s/DNSNAME/$1-dev.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
-    aws route53 change-resource-record-sets --hosted-zone-id Z04529243PF3H80RPJQ0J --change-batch file://tmp/record.json | jq  &>/dev/null
+    sed -e "s/DNSNAME/$1-dev.myhostedzone/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
+    aws route53 change-resource-record-sets --hosted-zone-id Z0576540AOW7M5U5BGNC --change-batch file://tmp/record.json | jq  &>/dev/null
 }
 
 CREATE() {
   COUNT=$(aws ec2 describe-instances --filters  "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null  | wc -l)
 
   if [ $COUNT -eq 0 ]; then
-    aws ec2 run-instances --launch-template LaunchTemplateId=lt-064de8ca52721fc8c,Version=2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$1}]" | jq &>/dev/null
+    aws ec2 run-instances --launch-template LaunchTemplateId=lt-064de8ca52721fc8c --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$1}]" | jq &>/dev/null
   else
     echo -e "\e[1;33m$1 Instance already exists\e[0m"
     UPDATE_DNS_RECORDS $1
